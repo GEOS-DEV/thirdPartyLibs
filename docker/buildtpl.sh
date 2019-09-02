6b33f704f8f4241d83d59c3d834cbaaffdcda311
Randolph R. Settgast <settgast1@llnl.gov>
2019-09-02 00:29:02 -0700

    try to remove the setup.sh files from the docker build...and try to just use docker build instead of running the image with the volume post build

--- docker/buildtpl.sh
+++ docker/buildtpl.sh
@@ -2,12 +2,7 @@
 set -e
 set -x
 
-mkdir GEOSX
 cd thirdPartyLibs_repo
 python scripts/config-build.py -hc host-configs/default.cmake -bt Release -DNUM_PROC:STRING=2 --buildpath /home/geosx/thirdPartyLibs/build-default-release --installpath /home/geosx/thirdPartyLibs/install-default-release
 cd /home/geosx/thirdPartyLibs/build-default-release
 make 
-cd thirdPartyLibs_repo/docker/GEOSX .
-python scripts/config-build.py -hc host-configs/default.cmake -bt Release --buildpath /home/geosx/GEOSX/build-default-release --installpath /home/geosx/GEOSX/install-default-release -DGEOSX_TPL_ROOT_DIR:PATH=/home/geosx/thirdPartyLibs/install-default-release
-cd /home/geosx/GEOSX/build-default-release
-make
