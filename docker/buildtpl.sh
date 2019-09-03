29f57fbf80c0304353de74e0042f01f48305b9ae
Randolph R. Settgast <settgast1@llnl.gov>
2019-09-03 01:11:54 -0700

    copy Dockerfile to set scope rather than copy entire tpl dir

--- docker/buildtpl.sh
+++ docker/buildtpl.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-cd thirdPartyLibs_repo
 python scripts/config-build.py -hc host-configs/default.cmake -bt Release -DNUM_PROC:STRING=2 --buildpath /home/geosx/thirdPartyLibs/build-default-release --installpath /home/geosx/thirdPartyLibs/install-default-release
 cd /home/geosx/thirdPartyLibs/build-default-release
 make 
