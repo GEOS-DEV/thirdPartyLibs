84e8a8a16d4f804d48cf69177bcd7b5f5d1665b8
Thomas GAZZOLA <j0436735@DN0a22c521.SUNet>
2019-09-11 15:36:10 -0700

    Environment variables for the build

--- docker/buildtpl.sh
+++ docker/buildtpl.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 cd thirdPartyLibs_repo
-python scripts/config-build.py -hc host-configs/default.cmake -bt Release -DNUM_PROC:STRING=2 --buildpath /home/geosx/thirdPartyLibs/build-default-release --installpath /home/geosx/thirdPartyLibs/install-default-release
-cd /home/geosx/thirdPartyLibs/build-default-release
+python scripts/config-build.py -hc host-configs/environment.cmake -bt Release -DNUM_PROC:STRING=2 --buildpath /home/geosx/thirdPartyLibs/build-environment-release --installpath /home/geosx/thirdPartyLibs/install-environment-release
+cd /home/geosx/thirdPartyLibs/build-environment-release
 make hdf5 conduit axom silo chai raja fparser mathpresso pugixml metis parmetis superlu_dist hypre uncrustify petsc trilinos
 cd ..
