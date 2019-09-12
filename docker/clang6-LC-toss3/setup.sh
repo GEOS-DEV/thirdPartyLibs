c7a1acd4f782d68b29a27eb3bfac7f4bef50df6e
Randolph Settgast <settgast1@llnl.gov>
2019-09-11 19:05:28 -0700

    Update the docker configuration (#55)
    * updated some of the docker configuration files
    
    * trying to remove env IMG from travis file
    
    * cloning main repo for compilation check after tpl compilation
    
    * try to remove the setup.sh files from the docker build...and try to just use docker build instead of running the image with the volume post build
    
    * add buildgeosx.sh script
    
    * copy Dockerfile to set scope rather than copy entire tpl dir
    
    * clone repo in docker image
    
    * re-enabled all tests
    
    * git lfs install for ubuntu builds
    
    * use copy instead of cloning internal to the container
    
    * turn off verbose make for tpls
    
    * simplifications of docker images. disable geosx build for now

--- docker/clang6-LC-toss3/setup.sh
+++ docker/clang6-LC-toss3/setup.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-sudo yum -y install bison flex
 sudo yum -y install clang-6.0.1-1-1.tce.ch6_2.x86_64.rpm
 
 rm /home/geosx/clang-6.0.1-public-1-1.tce.ch6_2.x86_64.rpm
--- docker/clang7-LC-toss3/setup.sh
+++ docker/clang7-LC-toss3/setup.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-sudo yum -y install bison flex
 sudo yum -y install clang-7.0.0-1-1.tce.ch6_2.x86_64.rpm
 
 rm /home/geosx/clang-7.0.0-public-1-1.tce.ch6_2.x86_64.rpm
