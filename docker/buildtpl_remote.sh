6d1516f65beffffb2e0cbb6b084bec4c9177a5dc
TotoGaz <49004943+TotoGaz@users.noreply.github.com>
2019-09-13 10:38:56 -0700

    Environment variables for the build (#59)
    Environment variables for the build

--- docker/buildtpl_remote.sh
+++ docker/buildtpl_remote.sh
@@ -6,9 +6,9 @@ git clone  --recurse-submodules --depth 1 https://github.com/GEOSX/thirdPartyLib
 cd thirdPartyLibs
 git lfs install
 git lfs pull
-python scripts/config-build.py -hc host-configs/default.cmake -bt Release -DNUM_PROC:STRING=3
-cd build-default-release
+python scripts/config-build.py -hc host-configs/environment.cmake -bt Release -DNUM_PROC:STRING=3
+cd build-environment-release
 make
 cd ..
 git submodule deinit .
-rm -rf build-default-release
+rm -rf build-environment-release
