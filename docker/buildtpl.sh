e09011ee411137a0cc394bbba7d29ce2c05e83f2
TotoGaz <49004943+TotoGaz@users.noreply.github.com>
2019-11-25 09:05:25 -0800

    Docker and HomeBrew improvements (#81)
    Reorganization of the docker and travis components.
    
    TPL are now referred through identifier ${TRAVIS_PULL_REQUEST}-${TRAVIS_BUILD_NUMBER} for both docker and osx tarball.
    Definition of a dedicated TRILINOS_BUILD_COMMAND that may help building the memory demanding trilinos.
    Fixing a precise release of HomeBrew (for open-mpi).
    Having pt-scotch as part of the tplMirror.

--- docker/buildtpl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-
-python scripts/config-build.py -hc host-configs/environment.cmake -bt Release -DNUM_PROC:STRING=2 --buildpath /home/geosx/thirdPartyLibs/build-environment-release --installpath /home/geosx/thirdPartyLibs/install-environment-release
-cd /home/geosx/thirdPartyLibs/build-environment-release
-make 
--- docker/clang7-LC-toss3/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-
-sudo yum -y install clang-7.0.0-1-1.tce.ch6_2.x86_64.rpm
-
-rm /home/geosx/clang-7.0.0-public-1-1.tce.ch6_2.x86_64.rpm
-rm /home/geosx/clang-7.0.0-1-1.tce.ch6_2.x86_64.rpm
