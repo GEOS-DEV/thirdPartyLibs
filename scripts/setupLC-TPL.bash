9c2b4b485b374e8f83df8a1cc5e3d5277ca39384
Benjamin Curtice Corbett <corbett5@llnl.gov>
2020-03-23 20:55:59 -0700

    Squash

--- scripts/setupLC-TPL.bash
+++ scripts/setupLC-TPL.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
-python scripts/config-build.py -hc ../GEOSX/host-configs/LLNL/toss_3_x86_64_ib-clang\@9.0.0.cmake -bt Release -ip /usr/gapps/GEOSX/thirdPartyLibs/2019-11-19/install-toss_3_x86_64_ib-clang\@9.0.0-release -DENABLE_UNCRUSTIFY:BOOL=OFF
-python scripts/config-build.py -hc ../GEOSX/host-configs/LLNL/toss_3_x86_64_ib-gcc\@8.1.0.cmake   -bt Release -ip /usr/gapps/GEOSX/thirdPartyLibs/2019-11-19/install-toss_3_x86_64_ib-gcc\@8.1.0-release   -DENABLE_UNCRUSTIFY:BOOL=OFF
-python scripts/config-build.py -hc ../GEOSX/host-configs/LLNL/toss_3_x86_64_ib-icc\@19.0.cmake    -bt Release -ip /usr/gapps/GEOSX/thirdPartyLibs/2019-11-19/install-toss_3_x86_64_ib-iccg\@19.0-release -DENABLE_UNCRUSTIFY:BOOL=OFF
 
-python scripts/config-build.py -hc ../GEOSX/host-configs/LLNL/lassen-clang\@upstream-NoMPI.cmake -bt Release -ip /usr/gapps/GEOSX/thirdPartyLibs/2019-11-19/install-lassen-clang@upstream-NoMPI-release -DENABLE_UNCRUSTIFY:BOOL=OFF
-python scripts/config-build.py -hc ../GEOSX/host-configs/LLNL/lassen-clang\@upstream.cmake       -bt Release -ip /usr/gapps/GEOSX/thirdPartyLibs/2019-11-19/install-lassen-clang@upstream-release       -DENABLE_UNCRUSTIFY:BOOL=OFF
+# Usage ./setupLC-TPL.bash pathToGeosxDirectory pathToInstallDirectory [extra rguments to config-build ]
+shift
+shift
+python scripts/config-build.py -hc $1/host-configs/LLNL/toss_3_x86_64_ib-clang\@9.0.0.cmake -bt Release -ip $2/install-toss_3_x86_64_ib-clang\@9.0.0-release $@
+python scripts/config-build.py -hc $1/host-configs/LLNL/toss_3_x86_64_ib-gcc\@8.1.0.cmake   -bt Release -ip $2/install-toss_3_x86_64_ib-gcc\@8.1.0-release   $@
+python scripts/config-build.py -hc $1/host-configs/LLNL/toss_3_x86_64_ib-icc\@19.0.cmake    -bt Release -ip $2/install-toss_3_x86_64_ib-iccg\@19.0-release $@
+
+python scripts/config-build.py -hc $1/host-configs/LLNL/lassen-clang\@upstream-NoMPI.cmake  -bt Release -ip $2/install-lassen-clang@upstream-NoMPI-release $@
+python scripts/config-build.py -hc $1/host-configs/LLNL/lassen-clang\@upstream.cmake        -bt Release -ip $2/install-lassen-clang@upstream-release       $@
