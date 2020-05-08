e88e47109e2a2369d96ce92b3af7052fcd1b79e8
Randolph R. Settgast <settgast1@llnl.gov>
2020-05-07 22:53:49 -0700

    Specify ECP bank for TPL builds on quartz

--- scripts/setupLC-TPL.bash
+++ scripts/setupLC-TPL.bash
@@ -25,9 +25,9 @@ mv build-* toBeDeleted
 rm -rf toBeDeleted &
 
 echo "Building all LC TPLs from $GEOSX_DIR to be installed at $INSTALL_DIR"
-./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz gcc@8.1.0   "srun -N 1 -t 60 -n 1" $@ &
-./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz clang@9.0.0 "srun -N 1 -t 60 -n 1" $@ &
-./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz icc@19.0.4  "srun -N 1 -t 60 -n 1" $@ &
+./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz gcc@8.1.0   "srun -N 1 -t 60 -n 1 -A geosecp" $@ &
+./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz clang@9.0.0 "srun -N 1 -t 60 -n 1 -A geosecp" $@ &
+./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz icc@19.0.4  "srun -N 1 -t 60 -n 1 -A geosecp" $@ &
 
 ./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR lassen clang@upstream       "lalloc 1 -qpdebug" $@ &
 ./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR lassen clang@upstream-NoMPI "lalloc 1 -qpdebug" $@ &
