d5e95b4761590a6327b30fea5b8c1fc8b376d9e6
Thomas Gazzola <thomas.gazzola@total.com>
2020-09-09 23:01:46 +0000

    loglevel again

--- scripts/download_prerequisites.py
+++ scripts/download_prerequisites.py
@@ -117,7 +117,7 @@ def main():
 if __name__ == "__main__":
     logging.basicConfig(format='[%(asctime)s][%(levelname)8s] %(message)s',
                         datefmt='%Y/%m/%d %H:%M:%S',
-                        level=logging.INFO)
+                        level=logging.DEBUG)
     main()
 
 # NOTES
