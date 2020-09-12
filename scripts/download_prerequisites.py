688d81d0ec816f704375c36c37b10218373e5f71
Thomas Gazzola <thomas.gazzola@total.com>
2020-09-12 00:50:39 +0000

    python doc

--- scripts/download_prerequisites.py
+++ scripts/download_prerequisites.py
@@ -20,12 +20,30 @@ class ErrorCode(enum.Enum):
 
 
 def read_config_file(file_name):
+    """
+    Parses and returns the file describing the TPLs.
+
+    Parameters
+        file_name (str): The path to the file.
+
+    Returns:
+        The parsed yaml, used as a dict.
+    """
     with open(file_name, 'r') as f:
         return yaml.load(f)
         # return yaml.load(f, Loader=yaml.FullLoader)
 
 
 def parse_args():
+    """
+    Parse the command line arguments
+
+    Returns:
+        A structure with 3 attributes (`tpl`, `dest`, `overwrite`) definind respectively
+        - the path to the tpl yaml description file,
+        - the download destination folder
+        - and whether we should overwrite a file if it already exists.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--tpl", default="tpls.yaml", help="Path to TPLs yaml description.")
     parser.add_argument("--dest", default="../tplMirror", help="Download directory.")
@@ -34,6 +52,19 @@ def parse_args():
 
 
 def validate_hashcode(file_name, md5_reference):
+    """
+    Computes the md5 checksum for given file and validate against reference.
+
+    Arguments:
+        file_name (str): Path to the file to be analyzed.
+        md5_reference (str): Hexdigest provided as refence as a 32 characters [0-9a-f]
+
+    Returns:
+        ErrorCode.Success if check matches. ErrorCode.Error in any other case.
+    
+    Raises:
+        Does not raise.
+    """
     md5 = hashlib.md5()
     chunk_size = md5.block_size * 1024
     try:
@@ -57,9 +88,14 @@ def validate_hashcode(file_name, md5_reference):
 def build_output_name(tpl, response, url):
     """
     Builds and returns the downloaded file name.
-    tpl: dict like - may contain the name in the `output` key.
-    response: http request answer - my contain a name in its header.
-    url: string - download link.
+
+    Arguments:
+        tpl (dict like): May contain the name in the `output` key to define a potential output file name.
+        response (http request answer): May contain a name in its header.
+        url (str): The download link.
+
+    Returns:
+        The download file name as a str.
     """
     # If a name is provided by the user
     if "output" in tpl:
@@ -74,6 +110,24 @@ def build_output_name(tpl, response, url):
 
 
 def download_tpl(tpl, dest, overwrite, chunk_size=1024):
+    """
+    Downloads the third partylib from `tpl`.
+    Validate against md5 checksums if provided.
+
+    Arguments:
+        tpl (dict like): Contains keys `url` that defines where to download from, 
+            `output` that defines where to doanload and potentially `md5` that contains a checksum reference and
+        dest (str): Doanload file name.
+        overwrite (bool): Shall we overwrite any file that already exists.
+        chunk_size (int): Size of the chunk to write when downloading the stream.
+            Technical argument, shoud not be needed to modify.
+    
+    Returns:
+        ErrorCode.Success if download and check are OK. ErrorCode.Error in any other case.
+    
+    Raises:
+        Does not raise.
+    """
     url = tpl.get("url")
 
     if not url:
@@ -84,6 +138,7 @@ def download_tpl(tpl, dest, overwrite, chunk_size=1024):
 
     try:
         with requests.Session() as session:
+            # Allow for some retries if a download fails on potentially recoverable or temporary errors.
             retry = Retry(total=2, backoff_factor=5, method_whitelist=False,
                           status_forcelist=(408, 429, 500, 502, 503, 504))
             adapter = HTTPAdapter(max_retries=retry)
@@ -97,6 +152,7 @@ def download_tpl(tpl, dest, overwrite, chunk_size=1024):
             output_file_name = build_output_name(tpl, response, url)
             output = os.path.join(dest, output_file_name)
 
+            # Overwrite part
             if os.path.exists(output):
                 if overwrite:
                     msg = 'File "%s" already exists, overwriting.' % output
@@ -106,6 +162,7 @@ def download_tpl(tpl, dest, overwrite, chunk_size=1024):
                     logging.warning(msg)
                     return ErrorCode.Success
 
+            # Actual downloading part
             with open(output, "wb") as f:
                 logging.info("Downloading %s to %s" % (url, output))
                 for chunk in response.iter_content(chunk_size=chunk_size):
@@ -121,6 +178,20 @@ def download_tpl(tpl, dest, overwrite, chunk_size=1024):
 
 
 def download_all_tpls(tpls, dest, overwrite):
+    """
+    Downloads all the third partylib from `tpls`.
+
+    Arguments:
+        tpls (iterable): Description of all the third party libraries. See `download_tpl` documentation.
+        dest (str): Doanload file name.
+        overwrite (bool): Shall we overwrite any file that already exists.
+    
+    Returns:
+        ErrorCode.Success if everything went OK for all tpls. ErrorCode.Error in any other case.
+    
+    Raises:
+        Does not raise.
+    """
     if not os.path.isdir(dest):
         error_msg = "Destination folder \"%s\" does not exist or is not a directory." % dest
         logging.error(error_msg)
