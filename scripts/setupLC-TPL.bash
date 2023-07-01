d52fdb5be0d05a2c239dc522c9ef921a62dbb3ea
Matteo Cusini <49037133+CusiniM@users.noreply.github.com>
2023-06-30 19:12:39 -0700

    Update raja - chai - vtk - fmt, cpp17 standard and modify images to use cuda11 and cmake 3.23 (#224)
    * Try using centos7-cuda11 nvidia image.
    
    * cuda11 on pecan image + renamed containers.
    
    * remove cpp14 standard
    
    * update Raja/Chai
    
    * added updated tarballs for chai, raja, vtk. tplMirror cleanup
    
    * upgrade fmt.
    
    * Update .travis.yml
    
    Co-authored-by: TotoGaz <49004943+TotoGaz@users.noreply.github.com>
    Co-authored-by: Randolph R. Settgast <settgast1@llnl.gov>

--- scripts/setupLC-TPL.bash
+++ scripts/setupLC-TPL.bash
@@ -27,11 +27,11 @@ rm -rf toBeDeleted &
 echo "Building all LC TPLs from $GEOSX_DIR to be installed at $INSTALL_DIR"
 chmod -R g+rx $INSTALL_DIR
 chgrp -R GEOS $INSTALL_DIR
-./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz clang@14 "srun -N 1 -t 90 -n 1 -A geosecp" $@ &
-./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz gcc@12 "srun -N 1 -t 90 -n 1 -A geosecp" $@ &
-./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR lassen clang@upstream       "lalloc 1 -qpdebug" $@ &
-./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR lassen clang13-cuda11       "lalloc 1 -qpdebug" $@ &
-
+./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz clang-14 "srun -N 1 -t 90 -n 1 -A geosecp" $@ &
+./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR quartz gcc-12 "srun -N 1 -t 90 -n 1 -A geosecp" $@ &
+./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR lassen gcc-8-cuda-11       "lalloc 1 -qpdebug" $@ &
+./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR lassen clang-13-cuda-11       "lalloc 1 -qpdebug" $@ &
+./scripts/setupLC-TPL-helper.bash $GEOSX_DIR $INSTALL_DIR lassen clang-10-cuda-11       "lalloc 1 -qpdebug" $@ &
 
 wait
 echo "Complete"
