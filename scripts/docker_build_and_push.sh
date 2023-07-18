2002fb108b9b8300b033b2c6e7035175f72363d1
Brian Manh Hien Han <han12@llnl.gov>
2023-07-18 11:44:26 -0700

    Move autofill line

--- scripts/docker_build_and_push.sh
+++ scripts/docker_build_and_push.sh
@@ -12,13 +12,13 @@ echo .git > .dockerignore
 DOCKER_TAG=${PULL_REQUEST_NUMBER}-${BUILD_NUMBER}
 echo "Docker tag is ${DOCKER_REPOSITORY}:${DOCKER_TAG}"
 
-INSTALL_DIR=${INSTALL_DIR_ROOT}-${PULL_REQUEST_NUMBER}-${BUILD_NUMBER}-${COMMIT:0:7}
+INSTALL_DIR=${INSTALL_DIR_ROOT:-/opt/GEOSX/GEOSX_TPL}-${PULL_REQUEST_NUMBER}-${BUILD_NUMBER}-${COMMIT:0:7}
 echo "Installation directory is ${INSTALL_DIR}"
 
 #docker build ${DOCKER_COMPILER_BUILD_ARG} \
 #--build-arg HOST_CONFIG=${HOST_CONFIG:-host-configs/environment.cmake} \
 #--build-arg DOCKER_ROOT_IMAGE=${DOCKER_ROOT_IMAGE:-undefined} \
-#--build-arg INSTALL_DIR=${INSTALL_DIR:-/opt/GEOSX_TPL} \
+#--build-arg INSTALL_DIR=${INSTALL_DIR} \
 #--tag ${DOCKER_REPOSITORY}:${DOCKER_TAG} \
 #--file ${DOCKERFILE} \
 #--label "org.opencontainers.image.created=$(date --rfc-3339=seconds)" \
