823ab96785da7a8f31c509fdc10183763c2ddd04
Xavier Lacoste <xl64100@gmail.com>
2023-08-16 03:41:01 +0200

    Pangea3 move from ENABLE_HYPRE_CUDA to ENABLE_HYPRE_DEVICE (#230)
    Co-authored-by: Xavier Lacoste <xavier.lacoste@totalenergies.com>

--- host-configs/TOTAL/pangea3-gcc8.4.1-openmpi-4.1.2.cmake
+++ host-configs/TOTAL/pangea3-gcc8.4.1-openmpi-4.1.2.cmake
@@ -103,6 +103,6 @@ set(PETSC_OMP_DIR ${GEOSX_TPL_ROOT_DIR}/omp-links-for-petsc CACHE STRING "")
 # PETSc doesn't seem to work correctly with clang.
 set(ENABLE_PETSC OFF CACHE BOOL "")
 set(ENABLE_HYPRE ON CACHE BOOL "")
-set(ENABLE_HYPRE_CUDA ON CACHE BOOL "")
+set(ENABLE_HYPRE_DEVICE "CUDA" CACHE STRING "")
 
 set(SCOTCH_NUM_PROC 1 CACHE STRING "" )
