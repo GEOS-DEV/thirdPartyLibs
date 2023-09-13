52d88d77c28e0b36bd872aac5d41429249dc406c
TotoGaz <49004943+TotoGaz@users.noreply.github.com>
2023-09-13 12:46:41 -0700

    Defining the default parameters in the yaml file. (#238)
    Correcting some installation dirs.

--- scripts/docker_build_and_push.sh
+++ scripts/docker_build_and_push.sh
@@ -12,19 +12,19 @@ echo .git > .dockerignore
 DOCKER_TAG=${PULL_REQUEST_NUMBER}-${BUILD_NUMBER}
 echo "Docker tag is ${DOCKER_REPOSITORY}:${DOCKER_TAG}"
 
-INSTALL_DIR=${INSTALL_DIR_ROOT}/GEOSX_TPL-${PULL_REQUEST_NUMBER}-${BUILD_NUMBER}-${COMMIT:0:7}
+INSTALL_DIR=${INSTALL_DIR_ROOT}/GEOS_TPL-${PULL_REQUEST_NUMBER}-${BUILD_NUMBER}-${COMMIT:0:7}
 echo "Installation directory is ${INSTALL_DIR}"
 
 docker build ${DOCKER_COMPILER_BUILD_ARG} \
---build-arg HOST_CONFIG=${HOST_CONFIG:-host-configs/environment.cmake} \
---build-arg DOCKER_ROOT_IMAGE=${DOCKER_ROOT_IMAGE:-undefined} \
+--build-arg HOST_CONFIG=${HOST_CONFIG} \
+--build-arg DOCKER_ROOT_IMAGE=${DOCKER_ROOT_IMAGE} \
 --build-arg INSTALL_DIR=${INSTALL_DIR} \
 --tag ${DOCKER_REPOSITORY}:${DOCKER_TAG} \
 --file ${DOCKERFILE} \
 --label "org.opencontainers.image.created=$(date --rfc-3339=seconds)" \
---label "org.opencontainers.image.source=https://github.com/GEOSX/thirdPartyLibs" \
+--label "org.opencontainers.image.source=https://github.com/GEOS-DEV/thirdPartyLibs" \
 --label "org.opencontainers.image.revision=${COMMIT}" \
---label "org.opencontainers.image.title=Building environment for GEOSX" \
+--label "org.opencontainers.image.title=Building environment for GEOS" \
 .
 
 docker push ${DOCKER_REPOSITORY}:${DOCKER_TAG}
