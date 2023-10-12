0aa5275bd38e958f335a183cf1ca562c714e8a92
wrtobin <tobin6@llnl.gov>
2023-10-12 11:43:17 -0700

    working on tioga testing

--- scripts/spack-install.sh
+++ scripts/spack-install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-CLUSTER=$1
-SYS_TYPE=$2
-NPROCS=$3
+SPACK_DIR=$1
+CLUSTER=$2
+SYS_TYPE=$3
+NPROCS=$4
 
 source spack/share/spack/setup-env.sh
 spack env create ${CLUSTER}-${SYS_TYPE} spack-environments/${CLUSTER}-${SYS_TYPE}/spack.yaml
 spack env activate ${CLUSTER}-${SYS_TYPE}
-spack install -j $NPROCS
+spack install -j ${NPROCS}
+despacktivate
