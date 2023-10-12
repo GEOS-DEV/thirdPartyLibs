e9017f7b46c61ce7c3d8665177ebada1433a2274
wrtobin <tobin6@llnl.gov>
2023-10-12 13:10:58 -0700

    working on tioga testing

--- scripts/spack-install.sh
+++ scripts/spack-install.sh
@@ -9,4 +9,4 @@ source spack/share/spack/setup-env.sh
 spack env create ${CLUSTER}-${SYS_TYPE} spack-environments/${CLUSTER}-${SYS_TYPE}/spack.yaml
 spack env activate ${CLUSTER}-${SYS_TYPE}
 spack install -j ${NPROCS}
-despacktivate
+spack env deactivate
