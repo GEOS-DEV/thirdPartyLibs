bf58453a23bdd28c328fba1ee927fa422df1d106
Victor A. P. Magri <50467563+victorapm@users.noreply.github.com>
2023-12-02 02:15:44 -0500

    Update hypre (#249)
    * Add script for downloading hypre from GitHub
    
    * Add new hypre tarball
    
    * Fix build warnings with SuperLU_Dist
    
    * update LC bank in build scripts

---
+++ scripts/updateHypre.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Input argument: hypre branch
+VERSION=${1:-"master"}
+
+# Local variables
+GEOSTPL_DIR=$(dirname $(dirname $(realpath $0)))
+TPL_MIRROR_DIR=${GEOSTPL_DIR}/tplMirror
+HYPRE_DIR=${GEOSTPL_DIR}/hypre-${VERSION}
+HYPRE_SRC_DIR=${HYPRE_DIR}/src
+
+# Fetch hypre repository
+cd ${GEOSTPL_DIR}
+rm -rf ${HYPRE_DIR}
+git clone https://github.com/hypre-space/hypre.git ${HYPRE_DIR}
+
+# Assign git variables
+cd ${HYPRE_DIR}
+git checkout ${VERSION}
+HYPRE_DEVELOP_STRING=$(git -C ${HYPRE_SRC_DIR} describe --match 'v*' --long --abbrev=9)
+HYPRE_DEVELOP_LASTAG=$(git -C ${HYPRE_SRC_DIR} describe --match 'v*' --abbrev=0)
+HYPRE_DEVELOP_NUMBER=$(git -C ${HYPRE_SRC_DIR} rev-list --count $HYPRE_DEVELOP_LASTAG..HEAD)
+HYPRE_DEVELOP_BRANCH=$(git -C ${HYPRE_SRC_DIR} rev-parse --abbrev-ref HEAD)
+
+# Replace placeholders in the "configure" file
+sed -i "s/\$develop_string/$HYPRE_DEVELOP_STRING/g"  ${HYPRE_SRC_DIR}/configure
+sed -i "s/\$develop_lasttag/$HYPRE_DEVELOP_LASTAG/g" ${HYPRE_SRC_DIR}/configure
+sed -i "s/\$develop_number/$HYPRE_DEVELOP_NUMBER/g"  ${HYPRE_SRC_DIR}/configure
+sed -i "s/\$develop_branch/$HYPRE_DEVELOP_BRANCH/g"  ${HYPRE_SRC_DIR}/configure
+sed -i "s/\$HYPRE_SRCDIR\/..\/.git/\$HYPRE_SRCDIR/g"   ${HYPRE_SRC_DIR}/configure
+
+# Remove git machinery from configure
+sed -i '/^[[:space:]]*develop_lastag=\$/d' ${HYPRE_SRC_DIR}/configure
+sed -i '/^[[:space:]]*develop_number=\$/d' ${HYPRE_SRC_DIR}/configure
+sed -i '/^[[:space:]]*develop_branch=\$/d' ${HYPRE_SRC_DIR}/configure
+sed -i '/^[[:space:]]*develop_string=\$/d' ${HYPRE_SRC_DIR}/configure
+
+# Remove hypre test data
+rm -rf ${HYPRE_DIR}/AUTOTEST
+rm -rf ${HYPRE_DIR}/src/test/TEST_*
+
+# Remove git folder
+rm -rf ${HYPRE_DIR}/.git
+
+# Create tarball and move it to tplMirror
+HYPRE_DIR=${GEOSTPL_DIR}/hypre-${HYPRE_DEVELOP_STRING}
+cd ${GEOSTPL_DIR}
+mv hypre-${VERSION} ${HYPRE_DIR}
+tar czvf hypre-${HYPRE_DEVELOP_STRING}.tar.gz hypre-${HYPRE_DEVELOP_STRING}
+mv hypre-${HYPRE_DEVELOP_STRING}.tar.gz ${TPL_MIRROR_DIR}
+
+# Remove temporary directory
+rm -rf ${HYPRE_DIR}
+
+# Update CMakeLists
+echo -e "Updating CMakeLists..."
+sed -i "s|set( HYPRE_URL \"\${TPL_MIRROR_DIR}/hypre-.*\.tar\.gz\" )|set( HYPRE_URL \"\${TPL_MIRROR_DIR}/hypre-${HYPRE_DEVELOP_STRING}.tar.gz\" )|" CMakeLists.txt
