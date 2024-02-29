cc3e37876af16e67d81d5849b9bac1bb1fe8299b
jay-a <jay.appleton@colorado.edu>
2024-02-29 07:32:44 -0800

    built all TPL library build functionality into scripts/setupLC-TPL.bash

--- scripts/setupLC-TPL.bash
+++ scripts/setupLC-TPL.bash
@@ -1,43 +1,85 @@
 #!/bin/bash
 
+## Builds the TPLs on all LC systems. Must be run from the top level TPL directory.
+## EXAMPLE ./setupLC-TPL.bash /user/workspace/username/GEOS clang@14 Release 
+
+# Default values
+USER_NAME=$(whoami)
+GEOS_DIR="/usr/workspace/${USER_NAME}/GEOS"
+BUILD_TYPE="Release"
+COMPILER="sysCompiler"
+ORIGINAL_DIR=$(pwd)
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -geosPath)
+            shift
+            GEOS_DIR="$1"
+            ;;
+        -bt)
+            shift
+            BUILD_TYPE="$1"
+            ;;
+        -comp)
+            shift
+            COMPILER="$1"
+            ;;
+        -h)
+            echo -e "Usage: 
+            \n    -geosPath \"PATH_TO_GEOS_ROOT\" :: default /usr/WS1/<username>/GEOS
+            \n    -bt \"BUILD_TYPE\"              :: default to Release (Release/Debug/RelWithDebInfo)
+            \n    -comp \"COMPILER\"              :: default SYS_COMPILER \n"
+            exit 1  
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Get the machine name from uname -i and strip trailing integers
+MACHINE=$(uname -n | sed 's/[0-9]*$//')
 # Get the Bash version
-bash_version=$(bash --version | grep -oE "[0-9]+\.[0-9]+" | head -n1)
+BASH_VERSION=$(bash --version | grep -oE "[0-9]+\.[0-9]+" | head -n1)
+
+GIT_REPO=$(git remote -v | awk '/origin.*\(push\)/ {print $2}')
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+GIT_COMMIT=$(git rev-parse --short HEAD)
+
+INSTALL_DIR="$GEOS_DIR/tplInstall-${MACHINE,,}-${COMPILER,,}-${BUILD_TYPE,,}"
+HOST_CONFIG=$GEOS_DIR/host-configs/LLNL/$MACHINE-$COMPILER.cmake
+
+LOG_FILE="TPLBuild-$MACHINE-$COMPILER-${BUILD_TYPE,,}.log"
+echo -e "\n---------------------------------------------------------------------------------------------------------------" > "$LOG_FILE"
+echo -e "\nGit Info:\n    - Repository URL: $GIT_REPO\n    - Repository Branch: $GIT_BRANCH\n    - Commit: $GIT_COMMIT \n" >> "$LOG_FILE" 
+echo -e "---------------------------------------------------------------------------------------------------------------\n" >> "$LOG_FILE"
+echo "Script path and name: $(pwd)/${0#./}" >> "$LOG_FILE"
+echo " " >> "$LOG_FILE"
+echo "Bash:           $BASH_VERSION" >> "$LOG_FILE"
+echo "User name:      $USER_NAME" >> "$LOG_FILE"
+echo "GEOS_DIR:       $GEOS_DIR" >> "$LOG_FILE"
+echo "Machine name:   $MACHINE" >> "$LOG_FILE"
+echo "Compiler:       $COMPILER" >> "$LOG_FILE"
+echo "Build type:     $BUILD_TYPE" >> "$LOG_FILE"
+echo "Install path:   $INSTALL_DIR" >> "$LOG_FILE"
+echo "host-config:    $HOST_CONFIG" >> "$LOG_FILE"
+echo " " >> "$LOG_FILE"
 
 # Compare the Bash version with 4
-if (( $(echo "$bash_version >= 4" | bc -l) )); then
-    echo "Bash version ${bash_version}."
-    echo " "
-else
-    echo "Error: Required Bash >= 4, current bash version is ${bash_version}."
-    echo " "
+if (( $(echo "$BASH_VERSION < 4" | bc -l) )); then
+    echo "Error: Required Bash >= 4, current bash version is ${BASH_VERSION}." >> "$LOG_FILE" 
+    echo " " >> "$LOG_FILE"
     exit 1
 fi
 
-git_repo=$(git remote -v | awk '/origin.*\(push\)/ {print $2}')
-git_branch=$(git rev-parse --abbrev-ref HEAD)
-git_commit=$(git rev-parse --short HEAD)
-
-## Builds the TPLs on all LC systems. Must be run from the top level TPL directory.
-## Usage ./setupLC-TPL.bash pathToGeosxDirectory machine compiler buildtype [extra arguments to config-build ]
-## EXAMPLE ./setupLC-TPL.bash /user/workspace/username/GEOS quartz clang@14 Release 
-GEOS_DIR=$1
-INSTALL_DIR=$GEOS_DIR
-MACHINE=$2
-COMPILER=$3
-BUILD_TYPE=$4
-
-LOG_FILE="TPLBuild-$MACHINE-$COMPILER-${BUILD_TYPE,,}.log"
-echo -e "\nGit Info:\n    - Repository URL: $git_repo\n    - Repository Branch: $git_branch\n    - Commit: $git_commit" > "$LOG_FILE" 
-echo -e "-------------------------------------------------------------------------------\n" >> "$LOG_FILE"
-echo "Script path and name: $(pwd)/$0" >> "$LOG_FILE"
-echo ""
-echo "Bash version ${bash_version}." >> $LOG_FILE
-echo " " >> $LOG_FILE
-echo -e "Script run using command:\n    $0 $*" >> $LOG_FILE
-
-## Eat up the command line arguments so the rest can be forwarded to setupLC-TPL-helper.
-shift
-shift
+if [ ! -f "$HOST_CONFIG" ]; then
+    echo -e "Error: Host config file ${HOST_CONFIG} does not exist. \n    Please correct this and try again." >> "$LOG_FILE" 
+    echo " " >> "$LOG_FILE"
+    exit 1
+fi
 
 ## Trap the interupt signal and kill all children.
 trap 'killall' INT
@@ -70,18 +112,39 @@ if [ -n "$(find . -maxdepth 1 -type d -name 'build-*' -print -quit)" ]; then
     echo "Deleted the toBeDeleted directory. " >> "$LOG_FILE"
 fi
 
-echo "Building all LC TPLs from $GEOS_DIR to be installed at $INSTALL_DIR" | tee -a "$LOG_FILE"
-chmod -R g+rx $INSTALL_DIR
-if [ "${MACHINE,,}" = "quartz" ]; then
-    echo " ./scripts/setupLC-TPL-helper.bash $GEOS_DIR $INSTALL_DIR $MACHINE $COMPILER $BUILD_TYPE \"srun -N 1 -t 90 -n 1\" " >> "$LOG_FILE"
-    ./scripts/setupLC-TPL-helper.bash $GEOS_DIR $INSTALL_DIR $MACHINE $COMPILER $BUILD_TYPE 'srun -N 1 -t 90 -n 1' #| tee -a "$LOG_FILE"
-elif [ "${MACHINE,,}" = "lassen" ]; then
-    echo " ./scripts/setupLC-TPL-helper.bash $GEOS_DIR $INSTALL_DIR $MACHINE $COMPILER $BUILD_TYPE \"lalloc 1 -qpdebug\" " >> "$LOG_FILE"
-    ./scripts/setupLC-TPL-helper.bash $GEOS_DIR $INSTALL_DIR $MACHINE $COMPILER $BUILD_TYPE 'lalloc 1 -qpdebug' #| tee -a "$LOG_FILE"  
-else    
-    echo "MACHINE: ${MACHINE,,} is currently not supported. "
+echo -e "Building all LC TPLs from $GEOS_DIR \n    to be installed at $INSTALL_DIR \n" >> "$LOG_FILE"
+if [ -d "$INSTALL_DIR" ]; then
+    echo -e "$INSTALL_DIR exists, \n   deleting its contents.\n" >> "$LOG_FILE"
+    rm -r "$INSTALL_DIR"
+else
+    echo -e "$INSTALL_DIR does not exist, \n    creating it and giving rx permisions.\n" >> "$LOG_FILE"
 fi
+mkdir $INSTALL_DIR
+# chmod -R +rx $INSTALL_DIR
 
-# wait
-# echo "Complete" | tee -a "$LOG_FILE"
-echo " ${MACHINE,,}-${BUILD_TYPE,,}"
+echo -e "---------------------------------------------------------------------------------------------------------------\n" >> "$LOG_FILE"
+module -T load cmake >> "$LOG_FILE" 2>&1
+echo " " >> "$LOG_FILE"
+
+python3 scripts/config-build.py -hc $HOST_CONFIG -bt $BUILD_TYPE -ip $INSTALL_DIR | tee -a "$LOG_FILE" 2>&1 
+cd build-$CONFIG-${BUILD_TYPE,,} 
+echo "changed to build-$CONFIG-${BUILD_TYPE,,} directory " | tee -a "$LOG_FILE" 2>&1
+cd .. | tee -a "$LOG_FILE" 2>&1
+echo "changed to ${ORIGINAL_DIR} directory " | tee -a "$LOG_FILE" 2>&1
+srun -N1 -t90 make | tee -a "$LOG_FILE" 2>&1
+# echo " $GET_A_NODE"
+# $GET_A_NODE make |
+
+# if [ "${MACHINE,,}" = "quartz" ]; then
+#     echo " ./scripts/setupLC-TPL-helper.bash $GEOS_DIR $INSTALL_DIR $MACHINE $COMPILER $BUILD_TYPE \"srun -N 1 -t 90 -n 1\" " >> "$LOG_FILE"
+#     ./scripts/setupLC-TPL-helper.bash $GEOS_DIR $INSTALL_DIR $MACHINE $COMPILER $BUILD_TYPE 'srun -N 1 -t 90 -n 1' #| tee -a "$LOG_FILE"
+# elif [ "${MACHINE,,}" = "lassen" ]; then
+#     echo " ./scripts/setupLC-TPL-helper.bash $GEOS_DIR $INSTALL_DIR $MACHINE $COMPILER $BUILD_TYPE \"lalloc 1 -qpdebug\" " >> "$LOG_FILE"
+#     ./scripts/setupLC-TPL-helper.bash $GEOS_DIR $INSTALL_DIR $MACHINE $COMPILER $BUILD_TYPE 'lalloc 1 -qpdebug' #| tee -a "$LOG_FILE"  
+# else    
+#     echo "MACHINE: ${MACHINE,,} is currently not supported. "
+# fi
+
+# # wait
+# # echo "Complete" | tee -a "$LOG_FILE"
+# echo " ${MACHINE,,}-${BUILD_TYPE,,}"
