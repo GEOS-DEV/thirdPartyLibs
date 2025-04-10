af8d6e80806f72e14dd7f7a40f5c3c980ca8e1d2
Brian Han <han12@llnl.gov>
2025-04-10 08:54:52 -0700

    ENABLE_PYLVARRAY unit tests when +pygeosx

--- scripts/spack_packages/packages/geosx/package.py
+++ scripts/spack_packages/packages/geosx/package.py
@@ -555,6 +555,7 @@ class Geosx(CMakePackage, CudaPackage):
 
             if '+pygeosx' in spec:
                 cfg.write(cmake_cache_option('ENABLE_PYGEOSX', True))
+                cfg.write(cmake_cache_option('ENABLE_PYLVARRAY', True))
             else:
                 cfg.write(cmake_cache_option('ENABLE_PYGEOSX', False))
 
