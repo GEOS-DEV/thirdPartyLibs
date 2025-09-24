10adf8cb17bc909cd476df90bb46dc1057e2a5ab
Randolph Settgast <settgast1@llnl.gov>
2025-09-24 10:49:40 -0700

    remove lassen. add matrix and tuo (#320)
    * remove lassen. add matrix
    
    * Add toss_4_x86_64_ib_cray spack config
    
    * Add rocm support to geos spack package
    
    * Update TPL scripts with tuolumne
    
    Co-authored-by: Victor A. P. Magri <paludettomag1@llnl.gov>

--- scripts/setupLC-TPL-uberenv.bash
+++ scripts/setupLC-TPL-uberenv.bash
@@ -41,15 +41,16 @@ fi
 
 echo "Building all LC TPLs from $GEOS_BRANCH to be installed at $INSTALL_DIR..."
 
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane gcc-12      "%gcc@12.1.1 +docs"   "salloc -n 112 --exclusive -p pdebug" $@ &
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane gcc-12noAVX "%gcc@12noAVX +docs"  "salloc -n 112 --exclusive -p pdebug" $@ &
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane gcc-13      "%gcc@13.3.1 +docs"   "salloc -n 112 --exclusive -p pdebug" $@ &
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane clang-14    "%clang@14.0.6 +docs" "salloc -n 112 --exclusive -p pdebug" $@ &
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR lassen gcc-8-cuda-11 "%gcc@8.3.1+cuda~uncrustify cuda_arch=70 ^cuda@11.8.0+allow-unsupported-compilers" "lalloc 1 -W 150" $@ &
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR lassen gcc-12-cuda-12 "%gcc@12.2.1+cuda~uncrustify cuda_arch=70 ^cuda@12.2.2+allow-unsupported-compilers" "lalloc 1 -W 150" $@ &
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR lassen clang-13-cuda-11 "%clang@13.0.1+cuda~uncrustify cuda_arch=70 ^cuda@11.8.0+allow-unsupported-compilers" "lalloc 1 -W 150" $@ &
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR lassen clang-10-cuda-11 "%clang@10.0.1+cuda~uncrustify cuda_arch=70 ^cuda@11.8.0+allow-unsupported-compilers" "lalloc 1 -W 150" $@ &
-./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR lassen clang-13-cuda-12 "%clang@13.0.1+cuda~uncrustify cuda_arch=70 ^cuda@12.2.2+allow-unsupported-compilers" "lalloc 1 -W 150" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane gcc-12      "%gcc@12.1.1 +docs"   "salloc -N 1 -n 112 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane gcc-12noAVX "%gcc@12noAVX +docs"  "salloc -N 1 -n 112 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane gcc-13      "%gcc@13.3.1 +docs"   "salloc -N 1 -n 112 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane clang-14    "%clang@14.0.6 +docs" "salloc -N 1 -n 112 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR dane clang-19    "%clang@19.1.3 +docs" "salloc -N 1 -n 112 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR matrix gcc-12-cuda-12.6   "%gcc@12.1.1+cuda~uncrustify   cuda_arch=90 ^cuda@12.6.0+allow-unsupported-compilers" "salloc -N 1 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR matrix gcc-13-cuda-12.9   "%gcc@13.3.1+cuda~uncrustify   cuda_arch=90 ^cuda@12.9.1+allow-unsupported-compilers" "salloc -N 1 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR matrix clang-14-cuda-12.6 "%clang@14.0.6+cuda~uncrustify cuda_arch=90 ^cuda@12.6.0+allow-unsupported-compilers" "salloc -N 1 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR matrix clang-19-cuda-12.9 "%clang@19.1.3+cuda~uncrustify cuda_arch=90 ^cuda@12.9.1+allow-unsupported-compilers" "salloc -N 1 --exclusive -t 120 -A vortex" $@ &
+./scripts/setupLC-TPL-uberenv-helper.bash $INSTALL_DIR tuolumne cce-20-rocm-6.4.2 "%cce@20.0.0 +rocm~pygeosx~trilinos~petsc~docs amdgpu_target=gfx942" "salloc -N 1 --exclusive -t 120 -A vortex" $@ &
 
 # Note: Estimated completion time is ~90 minutes.
 # Check log files for unreported completion of jobs.
@@ -59,4 +60,3 @@ chmod -R g+rx $INSTALL_DIR
 chgrp -R GEOS $INSTALL_DIR
 
 echo "Complete"
-
