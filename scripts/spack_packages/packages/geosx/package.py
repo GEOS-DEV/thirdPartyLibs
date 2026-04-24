2028c9820a6310b1064bd001b8e0e002629ac454
Randolph Settgast <randolph.settgast@earthflow.ai>
2026-04-24 00:52:30 -0700

    fix issues with spack on mac (#342)
    * fix issues with spack on mac
    
    * Removing gcc-runtime due to missing files on TTE/pine (#341)
    
    ---------
    
    Co-authored-by: Sushma Yellapragada <sushmayellapragada3@gmail.com>

--- scripts/spack_packages/packages/geosx/package.py
+++ scripts/spack_packages/packages/geosx/package.py
@@ -178,6 +178,7 @@ class Geosx(CMakePackage, CudaPackage, ROCmPackage):
     with when("+trilinos"):
         trilinos_packages = '+aztec+stratimikos~amesos2~anasazi~belos~ifpack2~muelu~sacado+thyra+zoltan'
         depends_on("trilinos@16.1.0 cflags='-fPIC' cxxflags='-fPIC -include cstdint' fflags='-fPIC'" + trilinos_packages)
+        depends_on("trilinos fflags='-fsecond-underscore'", when="platform=darwin")
         depends_on("trilinos~openmp", when="~openmp")
         depends_on("trilinos+openmp", when="+openmp")
 
@@ -219,6 +220,7 @@ class Geosx(CMakePackage, CudaPackage, ROCmPackage):
     #
     depends_on("mathpresso cxxflags='-fPIC'", when='+mathpresso')
     depends_on('grpc', when='+grpc')
+    depends_on('addr2line', when='+addr2line')
 
     # SPHINX_END_DEPENDS
 
@@ -656,7 +658,7 @@ class Geosx(CMakePackage, CudaPackage, ROCmPackage):
                 cfg.write('# addr2line\n')
                 cfg.write('#{0}\n\n'.format('-' * 80))
                 cfg.write(cmake_cache_option('ENABLE_ADDR2LINE', True))
-                cfg.write(cmake_cache_entry('ADDR2LINE_EXEC ', '/usr/bin/addr2line'))
+                cfg.write(cmake_cache_entry('ADDR2LINE_EXEC', os.path.join(spec['addr2line'].prefix.bin, 'addr2line')))
 
             cfg.write('#{0}\n'.format('-' * 80))
             cfg.write('# Other\n')
@@ -851,6 +853,7 @@ class Geosx(CMakePackage, CudaPackage, ROCmPackage):
                 cfg.write('# addr2line\n')
                 cfg.write('#{0}\n\n'.format('-' * 80))
                 cfg.write(cmake_cache_option('ENABLE_ADDR2LINE', True))
+                cfg.write(cmake_cache_entry('ADDR2LINE_EXEC', os.path.join(spec['addr2line'].prefix.bin, 'addr2line')))
 
     def cmake_args(self):
         pass
