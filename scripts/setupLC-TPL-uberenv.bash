715fdbc2b1f07f5df74814da3eaccf8caeee7cb9
Randolph Settgast <randolph.settgast@earthflow.ai>
2026-06-01 10:54:17 -0700

    Apply suggestion from @rrsettgast

--- scripts/setupLC-TPL-uberenv.bash
+++ scripts/setupLC-TPL-uberenv.bash
@@ -105,8 +105,8 @@ function launch_jobs() {
 
     tuo|tuolumne)
       ALLOC_CMD="salloc -N 1 --exclusive -p pdebug -t 60 -A vortex"
-      "${UBERENV_HELPER}" "$INSTALL_DIR" tuolumne cce-20-rocm-6.4.3  "+rocm~pygeosx~trilinos~petsc~docs+hypredrive amdgpu_target=gfx942 %%cce-20 ${COMMON}" "${ALLOC_CMD}" "$@" &
-      "${UBERENV_HELPER}" "$INSTALL_DIR" tuolumne llvm-amdgpu-6.4.3  "+rocm~pygeosx~trilinos~petsc~docs+hypredrive amdgpu_target=gfx942 %%llvm-amdgpu_6_4_3 ${COMMON}" "${ALLOC_CMD}" "$@" &
+      "${UBERENV_HELPER}" "$INSTALL_DIR" tuolumne cce-20-rocm-6.4.3  "+rocm~pygeosx~trilinos~petsc~docs amdgpu_target=gfx942 %%cce-20 ${COMMON}" "${ALLOC_CMD}" "$@" &
+      "${UBERENV_HELPER}" "$INSTALL_DIR" tuolumne llvm-amdgpu-6.4.3  "+rocm~pygeosx~trilinos~petsc~docs amdgpu_target=gfx942 %%llvm-amdgpu_6_4_3 ${COMMON}" "${ALLOC_CMD}" "$@" &
       ;;
 
     *)
